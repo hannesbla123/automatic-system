a75c93b9243760cd9636eb72c4c61f050d89ab88
hannesbla123 <johannes.vogel+bla@de.bosch.com>
2025-06-24 08:43:30 +0000

    add a few scripts

---
+++ my_script.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+set -e                          # <-- abort if any command fails
+
+echo "hello"                    # <-- print "hello" on stdout
+# exit 0                          # <-- exit with success
+exit 1                          # <-- exit with failure (anything != 0)
