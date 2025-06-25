0e91f8a66559f0dc425e2d7626a40125580ede46
hannesbla123 <johannes.vogel+bla@de.bosch.com>
2025-06-25 07:57:53 +0200

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -3,5 +3,5 @@
 set -e                          # <-- abort if any command fails
 
 echo "hello"                    # <-- print "hello" on stdout
-# exit 0                          # <-- exit with success
-exit 1                          # <-- exit with failure (anything != 0)
+exit 0                          # <-- exit with success
+# exit 1                          # <-- exit with failure (anything != 0)
