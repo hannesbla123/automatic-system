975890d430ddde691db41d0a5b98022b921f78dd
hannesbla123 <johannes.vogel+bla@de.bosch.com>
2025-06-26 10:24:13 +0000

    fix

--- list_codespace.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-CODESPACES_PAGE=webpage/codespaces.html
-
-gh codespace list > $CODESPACES_PAGE
---
+++ list_issues.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env sh
+PAGE=webpage/issues.html
+
+gh issue list > $PAGE
